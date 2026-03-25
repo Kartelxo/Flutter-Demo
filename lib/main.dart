f1cd7d7f0cab13bd1862288e7937b79f053f1014
Rodrigo <rodrigoetomas22@gmail.com>
2026-03-25 14:17:50 +0000

    Titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Primeira Aplicação Flutter',
+      title: 'O Meu Contador',
       theme: ThemeData(
         // This is the theme of your application.
         //
